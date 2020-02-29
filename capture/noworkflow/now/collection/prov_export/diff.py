1c5270b209d231683ee347456f93e3e247f65c79
Raffael Foidl <e11775820@student.tuwien.ac.at>
2020-02-29 14:05:42 +0100

    Add environment attribute diff to diff provo export

--- capture/noworkflow/now/collection/prov_export/diff.py
+++ capture/noworkflow/now/collection/prov_export/diff.py
@@ -1,10 +1,10 @@
 import prov.model as provo
 import prov.dot as provo_dot
 
+from noworkflow.now.collection.prov_export.environment_attrs import create_env_attr
 from noworkflow.now.collection.prov_export.module_deps import create_module_dep
-from noworkflow.now.persistence.models import Trial, Module
+from noworkflow.now.persistence.models import Trial, Module, EnvironmentAttr
 from noworkflow.now.persistence.models.diff import Diff as DiffModel
-from noworkflow.now.utils.functions import truncate
 from noworkflow.now.utils.io import print_msg
 
 
@@ -33,6 +33,42 @@ def module_changes(diff: DiffModel, document: provo.ProvDocument):
                                 [(provo.PROV_ROLE, "dependencyReplacement")])
 
 
+def environment_attr_changes(diff: DiffModel, document: provo.ProvDocument):
+    added, removed, replaced = diff.environment
+
+    for env_attr in added:  # type: EnvironmentAttr
+        create_env_attr(document, env_attr)
+        document.wasGeneratedBy("environmentAttribute{}".format(env_attr.id),
+                                "trial{}Execution".format(diff.trial2.id), None,
+                                "environmentAttribute{}AddAttr".format(env_attr.id),
+                                [(provo.PROV_ROLE, "environmentAttributeAddition")])
+
+    for env_attr in removed:  # type: EnvironmentAttr
+        create_env_attr(document, env_attr)
+        document.wasInvalidatedBy("environmentAttribute{}".format(env_attr.id),
+                                  "trial{}Execution".format(diff.trial2.id), None,
+                                  "environmentAttribute{}RemoveAttr".format(env_attr.id),
+                                  [(provo.PROV_ROLE, "environmentAttributeRemoval")])
+
+    for (attr_removed, attr_added) in replaced:  # type: EnvironmentAttr
+        create_env_attr(document, attr_added, suffix="_a")
+        document.wasGeneratedBy("environmentAttribute{}_a".format(attr_added.id),
+                                "trial{}Execution".format(diff.trial2.id), None,
+                                "environmentAttribute{}AddAttr".format(attr_added.id),
+                                [(provo.PROV_ROLE, "environmentAttributeAddition")])
+
+        create_env_attr(document, attr_removed, suffix="_r")
+        document.wasInvalidatedBy("environmentAttribute{}_r".format(attr_removed.id),
+                                  "trial{}Execution".format(diff.trial2.id), None,
+                                  "environmentAttribute{}RemoveAttr".format(attr_removed.id),
+                                  [(provo.PROV_ROLE, "environmentAttributeRemoval")])
+
+        document.wasRevisionOf("environmentAttribute{}_a".format(attr_added.id),
+                               "environmentAttribute{}_r".format(attr_removed.id),
+                               "trial{}Execution".format(diff.trial2.id), None, None, None,
+                               [(provo.PROV_ROLE, "environmentAttributeReplacement")])
+
+
 def export_diff(diff: DiffModel, args):
     document = provo.ProvDocument()
     document.set_default_namespace(args.defaultns)
@@ -44,6 +80,9 @@ def export_diff(diff: DiffModel, args):
 
     if args.modules:
         module_changes(diff, document)
+
+    if args.environment:
+        environment_attr_changes(diff, document)
 
     with open("hello.pn", "w") as file:
         document.serialize(destination=file, format="provn")
--- capture/noworkflow/now/collection/prov_export/environment_attrs.py
+++ capture/noworkflow/now/collection/prov_export/environment_attrs.py
@@ -5,16 +5,20 @@ from noworkflow.now.utils.functions import truncate
 from noworkflow.now.utils.io import print_msg
 
 
+def create_env_attr(document: provo.ProvBundle, env_attr: EnvironmentAttr, suffix=""):
+    document.entity("environmentAttribute{}{}".format(env_attr.id, suffix),
+                    [(provo.PROV_LABEL, env_attr.name),
+                     (provo.PROV_VALUE, truncate(env_attr.value)),
+                     (provo.PROV_TYPE, "environmentAttribute")])
+
+
 def export(trial: Trial, document: provo.ProvBundle):
     print_msg("Exporting environment conditions")
 
     collection = document.collection("environmentAttributes")
 
     for env_attr in trial.environment_attrs:  # type: EnvironmentAttr
-        document.entity("environmentAttribute{}".format(env_attr.id),
-                        [(provo.PROV_LABEL, env_attr.name),
-                         (provo.PROV_VALUE, truncate(env_attr.value)),
-                         (provo.PROV_TYPE, "environmentAttribute")])
+        create_env_attr(document, env_attr)
 
     for env_attr in trial.environment_attrs:  # type: EnvironmentAttr
         collection.hadMember("environmentAttribute{}".format(env_attr.id))
